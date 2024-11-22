2d9dc016456a5cb41b62f3cbb9f04ba42340653e
RAJVIR KUMAR <84075116+Rajvirkumar99@users.noreply.github.com>
2024-11-22 18:03:29 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from misreport import views  # Corrected import statement
+from misreport.views import  pageregister
+from misreport.piechart import Army_2076
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,11 +13,15 @@ urlpatterns = [
     path('extract-data/', views.extract_data_view, name='extract_data'),  # URL pattern for the extract data view
 
 
-    # path('extract-data/', views.extract_data_view, name='extract_data'),  # URL pattern for the extract data view
+    path('extract-data/', views.extract_data_view, name='extract_data'),  # URL pattern for the extract data view
     path('chart', views.linechart, name='chart-js'),
     path('userprofile', views.userprofile, name='userprofile'),
-    path('pageregister', views.pageregister, name='pageregister'),
+    path('pageregister/', pageregister, name='pageregister'),
+
     path('loginpage', views.loginpage, name='loginpage'),
+
+    # path('army_2076', piechart.Army_2076, name='army_2076'),
+   
 ]
 
 
